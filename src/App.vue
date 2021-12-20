6ecfef441513d9bbb6043a5340dcd4eaf8cffe7a
atmosphr <dev.guillaume.voiron@gmail.com>
2021-12-20 20:29:34 +0100

    Removed import of non-existing component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <template>
-    <example message="Hello, World!" />
+    Hello, World!
 </template>
 
 <script
     setup
     lang="ts"
 >
-import Example from './components/example.vue'
 </script>
 
 <style>
